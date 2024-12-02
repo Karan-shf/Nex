a083532d196b96d7f5ee2d1164403d6dea5ef2f3
Karan <k.shahkolaee@gmail.com>
2024-12-02 06:30:44 +0330

    gateway cors

--- Services/GateWay/index.js
+++ Services/GateWay/index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import morgan from "morgan";
+import cors from "cors"
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    exposedHeaders: ['x-auth-token'], 
+    credentials: true, 
+}));
 
 app.use("/IAM", createProxyMiddleware(
     {target: "http://localhost:8000", changeOrigin:true }
