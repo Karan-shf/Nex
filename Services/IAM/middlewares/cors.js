a84ca587062b13037e6f2383217be5efdbed1263
Karan <k.shahkolaee@gmail.com>
2024-12-02 07:31:59 +0330

    bug fix

--- Services/IAM/middlewares/cors.js
+++ Services/IAM/middlewares/cors.js
@@ -1,7 +1,7 @@
 export default function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-auth-token');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Expose-Headers', ['x-auth-token']);
     next();
