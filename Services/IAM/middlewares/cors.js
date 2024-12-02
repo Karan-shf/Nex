d08b7a259906bb3b36bc84ed5afcb8e1e74ad1e4
Karan <k.shahkolaee@gmail.com>
2024-12-02 05:47:04 +0330

    cors expose headers

--- Services/IAM/middlewares/cors.js
+++ Services/IAM/middlewares/cors.js
@@ -3,5 +3,6 @@ export default function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Expose-Headers', ['x-auth-token']);
     next();
 }
