e2dfe9348af715f41eda723478f87f496e1c292b
Karan-shf <k.shahkolaee@gmail.com>
2025-01-20 19:25:47 +0330

    followers and followings

--- Services/Core/Presentation/routes/following.js
+++ Services/Core/Presentation/routes/following.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { auth } from "../../middlewares/auth.js";
-import { toggleFollowing } from "../../Application/following.js";
+import { toggleFollowing, getUserFollowers, getUserFollowings } from "../../Application/following.js";
 
 const router = Router();
 
 router.post("/", auth, toggleFollowing);
+
+router.get("/userFollowers", auth, getUserFollowers);
+
+router.get("/userFollowings", auth, getUserFollowings);
 
 export default router;
