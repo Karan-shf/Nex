65479aa0abbb3c25c187a7a69d2e104fc22e42cc
Karan-shf <k.shahkolaee@gmail.com>
2025-01-21 08:09:27 +0330

    edit profile and grpc changes

--- Services/IAM/utilities/message_brokers/grpc.js
+++ Services/IAM/utilities/message_brokers/grpc.js
@@ -2,6 +2,7 @@ import grpc from '@grpc/grpc-js';
 import protoLoader from '@grpc/proto-loader';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import logger from '../loggers/generalLogger.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,4 +11,18 @@ const PROTO_PATH = path.join(__dirname, '../../../media.proto');
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 const mediaProto = grpc.loadPackageDefinition(packageDefinition).media;
 
-export default new mediaProto.MediaService('localhost:8087', grpc.credentials.createInsecure()); // export grpc client
+// export default new mediaProto.MediaService('localhost:8087', grpc.credentials.createInsecure()); // export grpc client
+const grpcClient = new mediaProto.MediaService('localhost:8087', grpc.credentials.createInsecure()); // export grpc client
+
+export default function uploadFileGrpc(grpcRequest) {
+    return new Promise((resolve, reject) => {
+        grpcClient.UploadFile(grpcRequest, (err, response) => {
+            if (err) {
+                logger.error('gRPC upload failed', err);
+                return reject(new Error('Failed to upload file.'));
+            }
+            logger.info("File uploaded via gRPC", response);
+            resolve(response);
+        });
+    });
+}
